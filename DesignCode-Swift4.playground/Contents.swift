1ca748e1ff3799ffdd5aaf248cb1fe3e660ee416
h01m3s <lin426797@gmail.com>
2018-05-01 16:07:44 -0400

    Animate in Playground

--- DesignCode-Swift4.playground/Contents.swift
+++ DesignCode-Swift4.playground/Contents.swift
@@ -3,3 +3,17 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// Go to Editor - Show Rendered Markup to show Markup
+//: # Heading
+//: ## Second level heading
+//: ### Third level heading
+
+//: Markdown text
+
+/*:
+ Markdown text over multiple lines.
+ */
+
+//: Google[Google](https://www.google.com)
+//: Logo: [Image](Logo.jpg)
--- DesignCode-UI.playground/Contents.swift
+++ DesignCode-UI.playground/Contents.swift
@@ -4,48 +4,101 @@ import UIKit
 import PlaygroundSupport
 
 class MyViewController : UIViewController {
+    
+    let cardView = UIView()
+    let titleLabel = UILabel()
+    let captionLabel = UILabel()
+    let coverImageView = UIImageView()
+    let backgroundImageView = UIImageView()
+    let descriptionLabel = UILabel()
+    let closeButton = UIButton()
+    
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
+        
+        cardView.frame = CGRect(x: 20, y: 255, width: 300, height: 250)
+        cardView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        cardView.layer.cornerRadius = 14
+        cardView.layer.shadowOpacity = 0.25
+        cardView.layer.shadowOffset = CGSize(width: 0, height: 10)
+        cardView.layer.shadowRadius = 10
 
-        let titleLabel = UILabel()
         titleLabel.frame = CGRect(x: 16, y: 16, width: 272, height: 38)
         titleLabel.text = "Learn Swift 4"
         titleLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         titleLabel.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
         
-        let captionLabel = UILabel()
         captionLabel.frame = CGRect(x: 16, y: 204, width: 272, height: 40)
         captionLabel.text = "Design Directly in Playground"
         captionLabel.textColor = .white
         
-        let coverImageView = UIImageView()
         coverImageView.frame = CGRect(x: 0, y: 0, width: 300, height: 250)
         coverImageView.contentMode = .scaleAspectFill
         coverImageView.image = #imageLiteral(resourceName: "Cover.jpg")
         coverImageView.clipsToBounds = true
         coverImageView.layer.cornerRadius = 14
         
-        let backgroundImageView = UIImageView()
         backgroundImageView.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
         backgroundImageView.image = #imageLiteral(resourceName: "Chapters Screen@2x.png")
         
-        let cardView = UIView()
-        cardView.frame = CGRect(x: 20, y: 255, width: 300, height: 250)
-//        cardView.backgroundColor = UIColor(red: 255/255, green: 45/255, blue: 45/255, alpha: 1)
-        cardView.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-        cardView.layer.cornerRadius = 14
-        cardView.layer.shadowOpacity = 0.25
-        cardView.layer.shadowOffset = CGSize(width: 0, height: 10)
-        cardView.layer.shadowRadius = 10
+        descriptionLabel.frame = CGRect(x: 20, y: 448, width: 335, height: 132)
+        descriptionLabel.text = "Three years ago, Apple completely revamped their design language for the modern users. It is now much simpler, allowing designers to focus on animation and function rather than intricate visual details."
+        descriptionLabel.textColor = .black
+        descriptionLabel.numberOfLines = 10
+        descriptionLabel.alpha = 0
+        
+        closeButton.frame = CGRect(x: 328, y: 20, width: 28, height: 28)
+        closeButton.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5)
+        closeButton.layer.cornerRadius = 14
+        closeButton.alpha = 0
+        closeButton.setImage(#imageLiteral(resourceName: "Action-Close@2x.png"), for: .normal)
+        closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(cardViewTapped))
+        cardView.addGestureRecognizer(tap)
+        cardView.isUserInteractionEnabled = true
         
         view.addSubview(backgroundImageView)
         view.addSubview(cardView)
         cardView.addSubview(coverImageView)
         cardView.addSubview(titleLabel)
         cardView.addSubview(captionLabel)
+        cardView.addSubview(descriptionLabel)
+        cardView.addSubview(closeButton)
         self.view = view
+    }
+    
+    @objc func cardViewTapped() {
+        let animator = UIViewPropertyAnimator(duration: 0.7, dampingRatio: 0.7) {
+            self.cardView.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
+            self.cardView.layer.cornerRadius = 0
+            self.coverImageView.frame = CGRect(x: 0, y: 0, width: 375, height: 420)
+            self.coverImageView.layer.cornerRadius = 0
+            self.titleLabel.frame = CGRect(x: 20, y: 20, width: 374, height: 38)
+            self.captionLabel.frame = CGRect(x: 20, y: 370, width: 272, height: 40)
+            self.descriptionLabel.alpha = 1
+            self.closeButton.alpha = 1
+        }
+        
+        animator.startAnimation(afterDelay: 1)
+    }
+    
+    @objc func closeButtonTapped() {
+        let animator = UIViewPropertyAnimator(duration: 0.7, dampingRatio: 0.7) {
+            self.cardView.frame = CGRect(x: 20, y: 255, width: 300, height: 250)
+            self.cardView.layer.cornerRadius = 14
+            self.coverImageView.frame = CGRect(x: 0, y: 0, width: 300, height: 250)
+            self.coverImageView.layer.cornerRadius = 14
+            self.titleLabel.frame = CGRect(x: 16, y: 16, width: 272, height: 38)
+            self.captionLabel.frame = CGRect(x: 16, y: 204, width: 272, height: 40)
+            self.descriptionLabel.alpha = 0
+            self.closeButton.alpha = 0
+        }
+        
+        animator.startAnimation(afterDelay: 1)
     }
 }
 // Present the view controller in the Live View window
+PlaygroundPage.current.needsIndefiniteExecution = true
 PlaygroundPage.current.liveView = MyViewController()

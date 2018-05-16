ef653d8a35409216b4d0fdcebb0e0b48e5a0a487
h01m3s <lin426797@gmail.com>
2018-05-16 00:12:15 -0400

    Parallax Animation

--- DesignCodeApp/ViewController.swift
+++ DesignCodeApp/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var deviceImageView: UIImageView!
-    @IBOutlet weak var playVisualView: UIVisualEffectView!
+    @IBOutlet weak var playVisualEffectView: UIVisualEffectView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var heroView: UIView!
+    @IBOutlet weak var bookView: UIView!
     
     @IBAction func playButtonTapped(_ sender: Any) {
         let urlString = "https://player.vimeo.com/external/235468301.hd.mp4?s=e852004d6a46ce569fcf6ef02a7d291ea581358e&profile_id=175"
@@ -29,16 +33,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        scrollView.delegate = self
         titleLabel.alpha = 0
         deviceImageView.alpha = 0
-        playVisualView.alpha = 0
+        playVisualEffectView.alpha = 0
         
         UIView.animate(withDuration: 1) {
             self.titleLabel.alpha = 1
             self.deviceImageView.alpha = 1
-            self.playVisualView.alpha = 1
+            self.playVisualEffectView.alpha = 1
         }
     }
 
+}
+
+extension ViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        if offsetY < 0 {
+            heroView.transform = CGAffineTransform(translationX: 0, y: offsetY)
+            playVisualEffectView.transform = CGAffineTransform(translationX: 0, y: -offsetY/3)
+            titleLabel.transform = CGAffineTransform(translationX: 0, y: -offsetY/3)
+            deviceImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/4)
+            backgroundImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/5)
+        }
+    }
+    
 }
 

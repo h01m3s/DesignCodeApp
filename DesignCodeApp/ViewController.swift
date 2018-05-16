48a3eb20a0cad97284710e022cf7e26aaee0821b
h01m3s <lin426797@gmail.com>
2018-05-15 23:42:54 -0400

    Storyboard and Code

--- DesignCodeApp/ViewController.swift
+++ DesignCodeApp/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var deviceImageView: UIImageView!
+    @IBOutlet weak var playVisualView: UIVisualEffectView!
+    
+    @IBAction func playButtonTapped(_ sender: Any) {
+        let urlString = "https://player.vimeo.com/external/235468301.hd.mp4?s=e852004d6a46ce569fcf6ef02a7d291ea581358e&profile_id=175"
+        let url = URL(string: urlString)
+        let player = AVPlayer(url: url!)
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        titleLabel.alpha = 0
+        deviceImageView.alpha = 0
+        playVisualView.alpha = 0
+        
+        UIView.animate(withDuration: 1) {
+            self.titleLabel.alpha = 1
+            self.deviceImageView.alpha = 1
+            self.playVisualView.alpha = 1
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 

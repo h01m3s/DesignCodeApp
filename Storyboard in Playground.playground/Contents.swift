35b95ce1fdebca57e9e2f0c1ddfe8bd67ab73266
h01m3s <lin426797@gmail.com>
2018-05-31 23:56:45 -0400

    Storyboard in Playground.

--- Storyboard in Playground.playground/Contents.swift
+++ Storyboard in Playground.playground/Contents.swift
@@ -1,6 +1,38 @@
 import PlaygroundSupport
 import UIKit
 
-class ViewController : CardController {}
+class ViewController : CardController {
+    
+    override func loadView() {
+        Bundle.main.loadNibNamed("View", owner: self, options: nil)
+    }
+    
+    override func tapCard(_ sender: AnyObject) {
+        NSLayoutConstraint.deactivate(cardConstraints)
+        closeButton.isHidden = false
+        
+        let animator = UIViewPropertyAnimator(duration: 0.7, dampingRatio: 0.7) {
+            self.closeButton.layer.opacity = 1
+            self.card.layer.cornerRadius = 0
+            self.card.layoutIfNeeded()
+        }
+        
+        animator.startAnimation()
+    }
+    
+    override func tapClose(_ sender: AnyObject) {
+        NSLayoutConstraint.activate(cardConstraints)
+        closeButton.isHidden = true
+        
+        let animator = UIViewPropertyAnimator(duration: 0.7, dampingRatio: 0.7) {
+            self.closeButton.layer.opacity = 0
+            self.card.layer.cornerRadius = 14
+            self.card.layoutIfNeeded()
+        }
+        
+        animator.startAnimation()
+    }
+    
+}
 
 PlaygroundPage.current.liveView = ViewController()
